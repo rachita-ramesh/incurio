9511ac15e5f340f7323aedb0dd590329545f4c7f
Ram311 <rachitaramesh52@gmail.com>
2025-02-18 19:16:27 -0800

    Added persistence for auth session

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,7 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false
+  }
+});
 
 export interface User {
   id: string;
